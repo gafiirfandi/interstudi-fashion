e090a64bcf0c539fd784d60516f640cabf77241b
Muhammad Destara Syanandi <muhammad.destara@ui.ac.id>
2021-08-21 13:26:49 +0700

    full finish

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import index
+from .views import index, tentangpengajar, programlainnya, faq, artikel, detailartikel, eventint, eventkeg, eventsem, pendaftaranonline
 
 urlpatterns = [
     path('', index, name="index"),
+    path('tentangpengajar', tentangpengajar, name="tentangpengajar"),
+    path('programlainnya', programlainnya, name="programlainnya"),
+    path('faq', faq, name="faq"),
+    path('pendaftaranonline', pendaftaranonline, name="pendaftaranonline"),
+    path('artikel', artikel, name="artikel"),
+    path('detailartikel', detailartikel, name="detailartikel"),
+    path('eventint', eventint, name="eventint"),
+    path('eventkeg', eventkeg, name="eventkeg"),
+    path('eventsem', eventsem, name="eventsem"),
+
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -3,3 +3,31 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
   return render(request, 'index.html')
+
+def tentangpengajar(request):
+  return render(request, 'tentangpengajar.html')
+
+
+def programlainnya(request):
+  return render(request, 'programlainnya.html')  
+
+def faq(request):
+  return render(request, 'faq.html')  
+
+def artikel(request):
+  return render(request, 'artikel.html')  
+
+def detailartikel(request):
+  return render(request, 'detailartikel.html')  
+
+def eventint(request):
+  return render(request, 'eventint.html')  
+
+def eventkeg(request):
+  return render(request, 'eventkeg.html')  
+
+def eventsem(request):
+  return render(request, 'eventsem.html')  
+
+def pendaftaranonline(request):
+  return render(request, 'pendaftaranonline.html')  
