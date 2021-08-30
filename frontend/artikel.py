01238fbddce86f518b5a92696babeed27ca2797b
M Aziz Gafirazi Irfandi <t-aziz.gafirazi@ovo.id>
2021-08-30 20:21:17 +0700

    add detail artikel

---
+++ frontend/artikel.py
@@ -0,0 +1,54 @@
+list_artikel = {
+  'tips_untuk_wanita_bertubuh_pendek' : {
+    'title': 'Tips Untuk Wanita Bertubuh Pendek',
+    'content': 'Memiliki tubuh mungil bukan berarti Anda tak bisa tampil stylish. Karena yang pertama Anda pikirkan pastilah masalah ukuran, model dan motif beragam produk fashion yang cenderung bikin tubuh semakin terlihat mungil. \
+      <br> \
+      <br> \
+      Jangan khawatir, Anda hanya memerlukan padanan busana dan pilihan beberapa produk fashion yang tepat agar penampilan Anda tetap terlihat keren. \
+      Yuk, sontek beberapa inspirasi mengenakan busana yang tepat bagi Anda si pemilik tubuh mungil yang dirangkum dari berbagai sumber berikut ini: \
+      <br> \
+      <br> \
+      1. Kenakan crop top \
+      <br> \
+      2. Pilih motif garis vertikal\
+      <br> \
+      3. Pakai rok mini dibandingkan rok panjang\
+      <br> \
+      4. Celana crop flare pas untuk Anda',
+    'url': 'tips_untuk_wanita_bertubuh_pendek'
+  },
+  'tips_menggunakan_pakaian_baru' : {
+    'title': 'Tips Menggunakan Pakaian Baru',
+    'content': 'Saat membeli pakaian di sebuah toko, pastinya Anda sudah tidak sabar untuk segera mengenakannya. Namun sayangnya tanpa disadari, bila Anda langsung mengenakan pakaian usai dibeli dapat memberikan dampak yang sangat buruk terhadap kesehatan kulit Anda. \
+      <br> \
+      Menggunakan pakaian baru tanpa mencucinya terlebih dahulu diyakini dapat merusak kulit. Hal ini dibenarkan oleh banyak ahli yang telah diuji dalam beberapa penelitian. \
+      <br> \
+      Dilansir dari Independent, seorang profesor dermatologi di University Medical Center Columbia, Donald Belsito, menyebutkan bahwa mengenakan pakaian baru tanpa dicuci terlebih dahulu dapat menyebabkan iritasi kulit, kudis, kutuan dan bahkan jamur.',
+    'url': 'tips_menggunakan_pakaian_baru'
+  },
+  '4_cara_ampuh_menjadi_fashion_designer' : {
+    'title': '4 Cara Ampuh Menjadi Fashion Designer',
+    'content': 'Punya hobi menggambar atau merancang busana? dan ingin menjadi fashion designer terkenal dengan karya dan rancangan yang dikenakan artis atau publik figur? Untuk menjadi seorang fashion designer tidak hanya cukup dengan modal bisa menggambar saja. Anda juga harus memiliki cara lain yang dapat mendukung cita-cita menjadi fashion designer terkenal. Berikut ini 4 cara ampuh menjadi fashion designer:\
+      <br> \
+      <br> \
+      <b>1. Memiliki Minat dan Hobi dalam Dunia Fashion</b>\
+      <br> \
+      Sudah jadi harga mati seorang fashion designer untuk menyukai, memiliki minat, dan hobi di dunia fashion. Dengan demikian pekerjaan akan menjadi terasa nikmat karena tidak ada rasa terpaksa dan beban. InterStudi sebagai salah satu lembaga pendidikan yang menyelenggarakan kursus desain busana di Jakarta dapat menjadi wadah bagi setiap orang guna menyalurkan minat dan hobi mereka dalam dunia fashion.\
+      <br> \
+      <br> \
+      <b>2. Jiwa Kreatif</b>\
+      <br> \
+      Kreatifitas yang tinggi memang jadi poin utama yang harus dimiliki. Sebagai seorang fashion designer, kreatifitas adalah hal yang paling dituntut dalam berkarya. Anda harus memiliki ide dan kreatifitas tinggi agar Anda bisa melahirkan produk - produk yang unik, berbeda, dan tentunya hasil rancangan Anda diminati banyak orang. Dalam proses belajar mengajar, siswa – siswi InterStudi di tuntut untuk dapat berkreatifitas dalam menghasilkan sebuah karya busana. Selain kreatif, rancangan siswa harus mengikuti pakem – pakem dalam pembuatan busana yang diajarkan oleh tim pengajar. Mas Adjie Notonegoro selaku Training Advisor program fashion desain 6 bulan InterStudi, menargetkan para siswa mampu menghasilkan minimal 2 karya fashion yang memiliki nilai artistik dan bernilai jual dipasaran.\
+      <br> \
+      <br> \
+      <b>3. Mengikuti Ajang Kompetisi, Peragaan Busana dan Exhibition</b>\
+      <br> \
+      Seorang designer dapat mengasah kemampuan dan menguji karyanya dengan mengikuti kompetisi mode. Dengan mengikuti ajang-ajang kompetisi peragaan busana, hingga exhibition merupakan waktu yang tepat untuk mengenalkan dan ‘menjual’ rancangan masing-masing designer pada masyarakat secara maksimal. Alumni dan siswa InterStudi berperan aktif dalam kegiatan tersebut, diantaranya pada ajang fashion show tahun 2015 di Epicentrum Kuningan, Jakarta Selatan.\
+      <br> \
+      <br> \
+      <b>4. Punya Ilmu, Pengetahuan dan Wawasan Luas dalam Dunia Fashion</b>\
+      <br> \
+      Profesi fashion designer bukan hanya pada masalah bisa menggambar saja, namun banyak teknik penting lainnya yang harus dikuasai tentang pola, bahan, dan trend. Karena itu, jika ingin menjalani profesi ini, maka setidaknya Anda pernah mengikuti sekolah mode ataupun kursus yang berhubungan erat dengan dunia fashion seperti di InterStudi. Pendidikan yang di tawarkan InterStudi yaitu selama 6 bulan, dengan kompetensi lulusannya harus mampu membuat 2 karya desain yang memiliki nilai artistik dan bernilai jual dipasaran. Ilmu, pengetahuan, keahlian, dan pengalaman yang didapatkan dari sekolah fashion InterStudi, dapat menjadi modal utama guna menjadi seorang fashion designer. Salah satu alasannya adalah karena tim pengajar InterStudi berasal dari para praktisi dunia mode tanah air, seperti Adjie Notonegoro, Ivan Gunawan, Billy Tjong, Dimas Mahendra, Leana Maulany, dll.',
+    'url': '4_cara_ampuh_menjadi_fashion_designer'
+  },
+}
--- frontend/views.py
+++ frontend/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 
+from .artikel import *
+
 # Create your views here.
 def index(request):
   return render(request, 'index.html')
 
 def tentangpengajar(request):
   return render(request, 'tentangpengajar.html')
-
 
 def programlainnya(request):
   return render(request, 'programlainnya.html')  
@@ -15,19 +16,28 @@ def faq(request):
   return render(request, 'faq.html')  
 
 def artikel(request):
-  return render(request, 'artikel.html')  
+  return render(request, 'artikel.html', {'list_artikel': list_artikel.values()})  
 
-def detailartikel(request):
-  return render(request, 'detailartikel.html')  
+def detailartikel(request, nama_artikel):
+  artikel = list_artikel[nama_artikel]
+  return render(request, 'detailartikel.html', {'artikel': artikel})  
 
 def eventint(request):
-  return render(request, 'eventint.html')  
+  fashion_show_2015_imgs = [('fashionshow2015_' + str(i) + '.jpeg') for i in range(1,9)]
+  fashion_show_2016_imgs = [('fashionshow2016_' + str(i) + '.jpeg') for i in range(1,15)]
+  fashion_show_2017_imgs = [('fashionshow2017_' + str(i) + '.jpeg') for i in range(1,20)]
+  return render(request, 'eventint.html', {'fashion_show_2015_imgs': fashion_show_2015_imgs, 'fashion_show_2016_imgs': fashion_show_2016_imgs, 'fashion_show_2017_imgs': fashion_show_2017_imgs})  
 
 def eventkeg(request):
-  return render(request, 'eventkeg.html')  
+  eventkeg_imgs = [('eventkeg_' + str(i) + '.jpeg') for i in range(1,21)]
+  return render(request, 'eventkeg.html', {'eventkeg_imgs': eventkeg_imgs})  
 
 def eventsem(request):
-  return render(request, 'eventsem.html')  
+  sem2015_imgs = [('seminar2015_' + str(i) + '.jpeg') for i in range(1,21)]
+  sem2014_imgs = [('seminar2014_' + str(i) + '.jpeg') for i in range(1,11)]
+  sem2016_imgs = [('seminar2016_' + str(i) + '.jpeg') for i in range(1,18)]
+  talkshow_imgs = [('talkshow_' + str(i) + '.jpeg') for i in range(1,7)]
+  return render(request, 'eventsem.html', {'sem2015_imgs': sem2015_imgs, 'sem2016_imgs': sem2016_imgs, 'sem2014_imgs': sem2014_imgs, 'talkshow_imgs': talkshow_imgs})  
 
 def pendaftaranonline(request):
   return render(request, 'pendaftaranonline.html')  
