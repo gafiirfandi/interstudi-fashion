639f5de9496bff0a99db683ba665ec1159d48ad9
Muhammad Destara Syanandi <muhammad.destara@ui.ac.id>
2021-08-30 23:20:30 +0700

    push 7 artikel terakhir

--- frontend/artikel.py
+++ frontend/artikel.py
@@ -51,4 +51,240 @@ list_artikel = {
       Profesi fashion designer bukan hanya pada masalah bisa menggambar saja, namun banyak teknik penting lainnya yang harus dikuasai tentang pola, bahan, dan trend. Karena itu, jika ingin menjalani profesi ini, maka setidaknya Anda pernah mengikuti sekolah mode ataupun kursus yang berhubungan erat dengan dunia fashion seperti di InterStudi. Pendidikan yang di tawarkan InterStudi yaitu selama 6 bulan, dengan kompetensi lulusannya harus mampu membuat 2 karya desain yang memiliki nilai artistik dan bernilai jual dipasaran. Ilmu, pengetahuan, keahlian, dan pengalaman yang didapatkan dari sekolah fashion InterStudi, dapat menjadi modal utama guna menjadi seorang fashion designer. Salah satu alasannya adalah karena tim pengajar InterStudi berasal dari para praktisi dunia mode tanah air, seperti Adjie Notonegoro, Ivan Gunawan, Billy Tjong, Dimas Mahendra, Leana Maulany, dll.',
     'url': '4_cara_ampuh_menjadi_fashion_designer'
   },
+
+  # mulai dari bagian bawah 
+  'tips_memakai_perhiasan_yang_tepat' : {
+    'title': 'Tips memakai perhiasan yang tepat',
+    'content':
+      '<h4><b>•Pertama:</b></h4> \
+      <br> \
+      Gunakan aksesoris sesuai busana yang dikenakan. Ketika bekerja, pilih aksesori yang tidak mencolok dengan desain yang simpel dan ukuran yang tidak terlalu besar. Dan untuk pesta gunakan aksesoris yang lebih menonjol, namun tidak heboh dan berlebihan. \
+      <br> \
+      <br> \
+      <h4><b>•Kedua:</b></h4> \
+      <br> \
+      Jika menggunakan anting dan kalung mungil, sesuaikan dengan tatanan rambut Anda. Bila rambut disanggul atau diikat, perhiasan tetap bisa ditonjolkan. \
+      <br> \
+      <br> \
+      <h4><b>•Ketiga:</b></h4> \
+      <br> \
+      Jika ingin menambahkan aksesoris misalnya gelang atau jam tangan, gunakanlah warna yang sama dan berbahan metal, agar Anda semakin terlihat cantik dan menawan. \
+      <br> \
+     ',
+    'url': 'tips_memakai_perhiasan_yang_tepat'
+  },
+  'tips_menggunakan_aksesoris_hijab_bagi_muslimah' : {
+    'title': 'Tips menggunakan aksesoris hijab bagi muslimah',
+    'content':
+      '<h4><b>1.Mengkombinasikan warna pakaian dengan aksesoris</b></h4> \
+      <br> \
+      Kebanyakan wanita yang berhijab menggunakan warna hijab yang senada dengan warna busana yang sedang digunakan. jika kamu memilih busana dengan warna yang netral seperti krem, putih atau hitam maka kamu dapat menggunakan aksesoris dengan warna yang sedikit lebih terang dan kontras dengan warna busana, sehingga aksesoris yang kamu gunakan akan menambah kesan lebih menarik pada penampilan kamu. Penggunaan aksesoris dengan warna terang ini dapat menunjang penampilan kamu terlihat lebih fresh dan tentu saja stylish. \
+      <br> \
+      <br> \
+      <h4><b>2.Sesuaikan dengan suasana, formal atau non-formal</b></h4> \
+      <br> \
+      Jika kamu memutuskan untuk meggunakan aksesoris dalam penampilanmu, tentu saja kamu harus menyesuaikannya dengan tema acaramu. Jika kamu ingin menghadiri acara yang formal, kamu dapat menggunakan beragam aksesoris dengan bahan metal agar terkesan lebih elegan. Jika kamu menghadiri acara yang lebih santai, kamu dapat menggunakan aksesoris dengan bahan korsase kain perca agar terkesan lebih santai. \
+      <br> \
+      <br> \
+      <h4><b>3.Sesuaikan dengan motif jilbab yang kamu gunakan</b></h4> \
+      <br> \
+      Ada banyak sekali motif jilbab yang ada di pasaran, mulai dari motif yang polos hingga motif yang agak ramai, seperti bunga-bunga dan juga motif abstrak. Dalam memilih aksesoris yang pas dan sesuai harus memperhatikan motif hijab yang sedang dipakai. Jika kamu menggunakan hijab dengan motif polos maka kamu dapat menambahkan beberapa aksesoris desain meriah sebagai pelengkap sehingga penampilan kamu tidak akan terkesan membosankan. \
+      <br> \
+      <br> \
+      <h4><b>4.Gunakan aksesoris sesuai dengan usia kamu</b></h4> \
+      <br> \
+      Nah, jangan sampai aksesoris yang kamu gunakan akan membuat tampilanmu semakin terlihat tua! Untuk kalangan remaja maka sebaiknya gunakan aksesoris yang memiliki corak warna terang dan meriah karena akan sesuai dengan kepribadian remaja yang aktif dan juga ceria. Jika kamu berada pada usia 25 tahun ke atas, maka kamu dapat menggunakan aksesoris dengan warna netral dan juga terlihat sedikit formal. \
+      <br> \
+      <br> \
+      Pada usia 25 tahun, seorang wanita cenderung memiliki kepribadian yang anggun sehingga pemilihan warna netral sangat pas dan sesuai dengan usia ini. Namun jika usiamu sudah mencapai umur 40 tahun ke atas, kamu dapat menggunakan aksesoris yang berbahan metal dengan desain sederhana, hindari menggunakan aksesoris yang terlalu meriah, karena pada usia ini seorang wanita akan terlihat lebih keibuan dan juga anggun.',
+    'url': 'tips_menggunakan_aksesoris_hijab_bagi_muslimah'
+  },
+  'tips_memilih_warna_baju_agar_tidak_terlihat_kusam' : {
+    'title': 'Tips memilih warna baju agar tidak terlihat kusam',
+    'content':
+      '<h4><b>Kulit Warna Putih</b></h4> \
+      <br> \
+      Bersyukurlah kamu bila memiliki kulit warna putih, karena hampir semua warna baju cocok untuk kamu pakai. Namun ingat ya ada dua warna yang lebih baik tidak kamu pakai, seperti kuning dan orange supaya kamu tidak terlihat pucat. \
+      <br> \
+      <br> \
+      <h4><b>Kulit Warna Sawo Matang</b></h4> \
+      <br> \
+      Memiliki warna kulit coklat mungkin cenderung sama halnya dengan memilih baju dengan kulit yang berwarna gelap atau hitam. Ada beberapa warna yang gak masuk di kulit kamu. Untuk warna yang baik, pas untuk warna kulitmu ialah hitam, merah, biru dan coklat muda. \
+      <br> \
+      <br> \
+      <h4><b>Kulit Warna Hitam</b></h4> \
+      <br> \
+      Kalau kamu memiliki warna kulit yang hitam atau gelap, ada baiknya gunakan baju berwarna cerah ya, misalnya warna biru, pink, putih atau abu-abu. Dan sebaliknya jangan memakai warna hitam, hijau gelap, coklat tua yang buat kulit kamu jadi makin terlihat kusam. \
+      <br> \
+      <br> \
+      ',
+    'url': 'tips_memilih_warna_baju_agar_tidak_terlihat_kusam'
+  },
+  'tips_berbusana_agar_tampak_elegan' : {
+    'title': 'Tips Berbusana Agar Tampak Elegan',
+    'content':
+      'Saat ini untuk terlihat berkelas dan elegan, Anda tidak perlu memakai barang-barang yang mahal. Namun Anda hanya perlu keluar dari zona aman dan membuat fesyen Anda sendiri saat berbusana. Selain itu, konsep berbusana turut menggambarkan selera mode dan bakat fesyen yang dimiliki. Berikut beberapa tips berbusana bagi perempuan untuk tampil elegan dilansir Boldsky. \
+      <br> \
+      <br> \
+      <h4><b>Bereksperimen dengan warna</b></h4> \
+      <br> \
+      Seringkali banyak wanita yang menerapkan aturan-aturan dalam berbusana, seperti membawa tas tangan harus serupa dengan warna busananya. Pada kenyataannya hal tersebut bukan hal yang wajib. Untuk itu, buatlah eksperimen unik dengan pakaian Anda, sebab hal ini akan membuat Anda terlihat berkelas. \
+      <br> \
+      <br> \
+      <h4><b>Memakai heels</b></h4> \
+      <br> \
+      Memakai heels dapat menambahkan kesan glamour pada penampilan. Anda juga terlihat lebih elegan dan berkelas ditambah Anda mengenakan pakaian yang lebih trendi, hal ini dapat meningkatkan rasa percaya diri Anda. \
+      <br> \
+      <br> \
+      <h4><b>Kacamata hitam</b></h4> \
+      <br> \
+      Sebuah kacamata hitam besar dapat menambahkan kualitas penampilan Anda. Tidak peduli pakaian apa yang Anda kenakan dengan memakai kacamata tentunya Anda akan terlihat lebih menarik. Tidak lupa untuk menerapkan lipstik merah agar Anda mendapatkan tampilan kasual dan berkelas. \
+      <br> \
+      <br> \
+      <h4><b>Merias diri sesuai pakaian</b></h4> \
+      <br> \
+      Gaun yang bagus, rambut dan kulit dapat memainkan peran penting untuk membuat Anda terlihat cantik. Sebuah gaun yang bagus akan terbuang begitu saja tanpa penataan rambut yang kurang baik. Anda juga perlu merias wajah Anda sesuai pakaian Anda. Untuk gaun yang sedikit berat, sebaiknya aplikasikan riasan yang lembut dan sederhana. \
+      <br> \
+      <br> \
+      <h4><b>Tambahkan potongan scart</b></h4> \
+      <br> \
+      Anda dapat menambahkan sebuah potongan scarf atau syal pada pakaian Anda dengan hanya menyelipkannya di leher Anda. Sebaiknya kenakan scarf dengan pakaian yang polos atau sebaliknya. \
+      <br> \
+      <br> \
+      <h4><b>Perhatikan aksesoris</b></h4> \
+      <br> \
+      Perlu diingat janganlah berlebihan saat mengenakan aksesori, karena hal itu dapat membuat Anda terlihat seperti toko berjalan. Pakailah beberapa cincin saja pada jari Anda. Jika Anda mengenakan kalung maka Anda dapat memilih untuk tidak memakai anting atau bisa memakainya dengan ukuran yang lebih kecil.',
+    'url': 'tips_berbusana_agar_tampak_elegan'
+  },
+  '5_tips_berbusana_menurut_ilmu_psikologi' : {
+    'title': '5 Tips Berbusana Menurut Ilmu Psikologi',
+    'content':
+      'Berpenampilan menarik bukan berarti menarik bukan berarti Anda harus mengikuti tren terkini atau berbalutkan busana keluaran desainer papan atas. \
+      <br> \
+      <br> \
+      Segala hal yang berkaitan dengan psikologi manusia seperti warna yang memancarkan aura positif atau motif yang meninggalkan kesan manis bagi orang yang melihatnya turut menentukan seberapa menariknya penampilan Anda. Berikut selengkapnya: \
+      <br> \
+      <br> \
+      <h4><b>1. Pilih Padanan yang Lebih Berani</b></h4> \
+      <br> \
+      Menurut hasil riset psikolog Kurt Gray dan timnya, manusia lebih memberi respon positif saat dihadapkan pada keseimbangan yang baik antara "ketidakserasian dan senada", atau "sederhana dan rumit". Maka itu, jangan takut untuk mengeksplor tampilan dengan paduan warna, tekstur atau motif yang berbeda. \
+      <br> \
+      <br> \
+      <h4><b>2. Pakai Warna Kuning</b></h4> \
+      <br> \
+      Warna yang satu ini memang tidak membuat kaki terlihat lebih jenjang atau pinggul yang ramping. Namun Karen J. Pine, professor psikologi Universitas Hertfordshire, Inggris, mengatakan kuning dapat memancarkan energi positif dan kebahagian. Siapapun akan senang melihat Anda. \
+      <br> \
+      <br> \
+      <h4><b>3. Kenakan Busana Berbahu Lebar</b></h4> \
+      <br> \
+      Busana dengan bahu yang lebar atau bidang, masih menurut Karen, menciptakan ilusi visual yang merampingkan pinggang. Dengan begitu, Anda akan terlihat lebih ramping. \
+      <br> \
+      <br> \
+      <h4><b>4. Tampil Serba Monokrom</b></h4> \
+      <br> \
+      Cara lain untuk "melangsingkan" tubuh secara instan, tampil lah dalam balutan busana serba satu warna. Dipaparkan dalam buku "Design & Technology" terbitan Pasca Press (2003), busana semacam itu dapat "menyamarkan garis-garis garis-garis pemisah". \
+      <br> \
+      <br> \
+      <h4><b>5. Lirik Juga Garis Diagonal</b></h4> \
+      <br> \
+      Sudah bukan rahasia lagi jika busana bermotif garis vertikal memberi kesan langsing dan horizontal melebarkan tampilan Anda. Lalu bagaimana dengan garis diagonal? Menurut sebuah riset, seperti dikutip Who What Wear, garis diagonal ampuh untuk menarik atensi pada bagian-bagian tertentu. Kardigan, blazer, coat, dan beberapa luaran lainnya adalah jenis busana yang hadir dengan garis diagonal pada bagian kancingnya.',
+    'url': '5_tips_berbusana_menurut_ilmu_psikologi'
+  },
+   'berikut_unsur_desain_fashion_yang_harus_kamu_ketahui' : {
+    'title': 'Berikut unsur desain fashion yang harus kamu ketahui',
+    'content': 
+      'Jika kamu menginginkan menjadi seorang desainer dan masih bingung harus memulai dari mana. Disinilah kamu kana mulai belajar, yaitu dengan mulai mempelajari unsur desain dan tekhniknya. Setelah kamu mulai mendesain maka kamu akan benar-benar merasakannya.\
+      <br> \
+      <br> \
+      Mungkin selama ini kamu masih mendesain segala pakaian dengan indah, namun kamu juga harus mempelajari unsur desain fashion yang harus kamu ketahui. Kamu dapat menciptakan karya desain yang luar biasa hanya dengan garis-garis yang akan terlihat menakjubkan. Dengan karya yang akan kamu hasilkan jika telah memenuhi syarat dan semua unsur di pakai pasti hasilnya akan sangat di kenal oleh publik. \
+      <br> \
+      <br> \
+      Memang tidak semua desainer dapat sukses dan terkenal, mungkin bukan karena hasil karyanya tidak bagus. Melainkan kurangnya pengetahuan yang membuat kurangnya unsur dan teknik dalam proses pembuatannya. Sebelum kamu mempelajari unsur arah dan unsur garis dalam desain yang kamu buat nanti, sebaiknya kamu membaca artikel ini terlebih dahulu. \
+      <br> \
+      <br> \
+      <h4><b>Berikut unsur desain fashion yang harus kemu ketahui:</b></h4> \
+      <br> \
+      <b>Bentuk naturalis</b> \
+      <br> \
+      Unsur desain yang pertama yaitu bentuk naturalis. Bentuk naturalis ini merupakan bentuk yang berasal dari bentuk alam seperti tumbuhan, pemandangan, hewan dan bentuk-bentuk alam lainnya. \
+      <br> \
+      <br> \
+      <b>Bentuk geometris</b> \
+      <br> \
+      Bentuk-bentuk geometris ini dapat di ukur menggunakan alat pengukur yang memiliki bentuk teratur. Bentuk geometris ini seperti segi tiga, segi empat dan bentuk-bentuk lainnya. \
+      <br> \
+      <br> \
+      Jika kamu dapat memankan imajinasimu maka kamu akan dapat mendesain ssuatu yang sangat indah meski berawal dari bentuk-bentuk geometris. Karena bentuk geometris dapat di buat sesuai dengan ukuran meski baru dalam bentuk gambar. \
+      <br> \
+      <br> \
+      <b>Bentuk dekoratif</b> \
+      <br> \
+      Bentuk dekoratif merupakan bentuk yang sudah dirubah dari bentuk asli yang melalui proses stilir atau stilasi yang masih terdapat ciri khas bentuk aslinya. Bentuk dekoratif dapat berupa ragam hias pada sulaman yang sudah dirubah menjadi bentuk yang lebih kental nilai seninya. Bentuk dekoratif ini lebih banyak digunakan sebagai menghias bidang atau benda tertentu. \
+      <br> \
+      <br> \
+      <b>Bentuk Abstrak</b> \
+      <br> \
+      <br> \
+      Unsur desain fashion yang harus kemu ketahui selanjutnya yaitu dengan bentuk abstrak. Bentuk ini sama sekali tidak terkait dengan apapun, namun jika kamu dapat memainkan imajinasimu kamu akan menghasilkan karya yang sangat menakjubkan. \
+      <br> \
+      <br> \
+      Pembuatan desain tidak hanya berlandaskan dengan ide semata, tetapi juga harus memiliki konsep rancangan bentuk yang mendasar agar mudah dipahami. Konsep rancangan bentuk dasar ini dapat dituangkan ke dalam bentuk pola rancangan, sehingga mudah untuk diwujudkan ke bentuk pakaian yang sesungguhnya.',
+    'url': 'berikut_unsur_desain_fashion_yang_harus_kamu_ketahui'
+  },
+   'tips_mudah_menjadi_seorang_desainer_sukses' : {
+    'title': 'Tips Mudah Menjadi Seorang Desainer Sukses',
+    'content': 'Profesi yang tidak memerlukan ijazah selanjutnya yaitu sebagai desainer, memang jika kamu menginginkan hasil yang lebih baik sebaiknya kamu sekolah desainer. Namun hal ini tidak wajib karena jika kamu telah memiliki passion dalam diri kamu sebagai seorang designer.\
+      <br> \
+      <br> \
+      Tips mudah menjadi desainer sukses memang harus dari keyakinan diri sendiri. Tekad yang kuad harus di mulai dari diri sendiri agar tujuan lebih cepat tercapai. Berbagai profesi jika di tekuni secra baik maka kamu akan mendapatkan hasil yang optimal. Ketika kamu telah memiliki bakat atau minat pada bidang fashion ini. Sering kali banyak yang bingung harus mulai dari mana.\
+      <br> \
+      <br> \
+      Jika kamu menginginkan untuk menekuni dalam bidang ini maka kamu harus memiliki langkah awal yang tepat. Jika dalam langkah wal saja kamu sudah salag bagaimana selanjutnya, mungkin kamu akan menghadapi kegagalan. Hidup harus terus berdiri ketika terjatuh sampai tuhan berkata waktunya kamu untuk pulang. Oleh karena itu kamu janngan mudah menyerah dan terus berusaha.\
+      <br> \
+      <br> \
+      <h4><b>Berikut tips mudah menjadi desainer sukses:</b> </h4>\
+      <br> \
+      <b>Niat</b> \
+      <br> \
+      Jika kamu sudah yakin pada diri kamu sendiri untuk menekuni bidang desainer ini. Jangan pernah lakukan setengah-setengah, optimalkan kemampuan yang kamu punya dan kuatkan tekad kamu. Namun jika kamu menggeluti bidang ini karena keinginan orang lain dan kamu kurang menyukainya segeralah mencari alasan untuk meninggalkannya. \
+      <br> \
+      <br> \
+      <b>Meningkatkan dan mengembangkan kemampuan</b> \
+      <br> \
+      Tips mudah menjadi designer sukses selanjutnya yaitu dengan meningkatkan dan mengembangkan kemampuan kamu dalam bidang fashion ini. Jika kamu sudah memiliki rasa cinta dan suka terhadap dunia fashion ini maka kamu akan dengan mudahnya untuk meningkatkan dan mengembangkan kemampuan. \
+      <br> \
+      <br> \
+      Biasanya jika kamu menginginkan menjadi seorang desainer yang handal maka kamu harus bisa menjahit dan memotong kain. Karena dunia fashion ini tak lepas dari dunia jahit dan kain. Kamu harus mengatahui kualitas kain yang bagus dan seagala peralatan yang digunakan untuk membuat baju. \
+      <br> \
+      <br> \
+      <b>Belajar lebih banyak lagi</b> \
+      Kamu dapat belajar dengan mengikuti sekolah desain atau dapat belajar autodidak pada orang yang sudah profesional dalam bidangnya. Biasanya jika kamu belajar pada orang yang sudah profesional dalam bidangnya kamu akan di ajari tips khusus dan rahaasia. \
+      <br> \
+      Namun jika kamu mengikuti sekolah fashion maka kamu akan mendapatkan kesempatan untuk belajar secara langsung pada perusahaan fahion. Karena sekolah biasanya sudah bekerjasama dengan perusahaan fashion tersebut. \
+      <br> \
+      <br> \
+      <b>Menentukan bidnag fashion yang di gemari</b> \
+      <br> \
+      Jika kamu menyukai bidang fashion aksesoris maka kamu harus dapat menciptakan karya seunik mungkin. Sehingga produk kamu akan mudah untuk diingat, namun bidang aksesoris ini lumayan cukup lama untuk dapat berkembang daripada bidang busana yang lebih mudah untuk bekembang secara pesat. \
+      <br> \
+      <br> \
+      <b>Tujuan menjadi desainer</b> \
+      <br> \
+      Jika kamu memiliki tujuan untuk sekses dalam bidang fashion maka kamu harus memiliki pemikiran seperti: \
+      <br> \
+      <br> \
+      • Pekerjaan ini merupakan suatu pekerjaan yang akan ditekuni dalam jangka waktu yang panjang. \
+      <br> \
+      • Ketidak pastian bukan menjadi suatu masalah yang besar karena percaya bahwa apa yang kamu lakukan adalah benar. \
+      <br> \
+      • Kamu selalu memiliki ide-ide yang cemerlang dan baru. \
+      <br> \
+      • Mampu mendengarkan dan memahami klien dengan baik. \
+      <br> \
+      • Kamu hidup, makan, dan bernafas dengan dunia fashion. \
+      <br> \
+      <br> \
+      Itulah tips mudah menjadi designer sukses, jika kamu akan terjun dala bidang fashion ini. Jangan pernah brhenti bereksperimen dan selalu up date tentang fashion di pasaran. Jika perlu malah justru kamu yang harus membuat style fashion baru agar orang mengenal produk kamu yang unik.',
+    'url': 'tips_mudah_menjadi_seorang_desainer_sukses'
+  },
 }
